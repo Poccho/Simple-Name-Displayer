f6bf3f1d7592ce9cf5c47d6ef2159c27a0ceed1b
Roger Anthony D. Bairoy <108564918+Poccho@users.noreply.github.com>
2024-02-05 14:18:39 +0800

    Simple name displayer

--- lib/main.dart
+++ lib/main.dart
@@ -2,131 +2,112 @@ import 'package:flutter/material.dart';
 import 'package:flutter_toastr/flutter_toastr.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'User Details App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
-        useMaterial3: true,
+        primaryColor: Colors.purpleAccent,
+        appBarTheme: AppBarTheme(
+          color: Colors.purpleAccent,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: UserDetailsScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
+class UserDetailsScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _UserDetailsScreenState createState() => _UserDetailsScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-  void _minusCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  void showTaost(){
-  }
-
-
+class _UserDetailsScreenState extends State<UserDetailsScreen> {
   TextEditingController firstNameController = TextEditingController();
   TextEditingController middleNameController = TextEditingController();
   TextEditingController lastNameController = TextEditingController();
+
+  void showDetailsSnackBar() {
+    String firstName = firstNameController.text;
+    String middleName = middleNameController.text;
+    String lastName = lastNameController.text;
+
+    if (firstName.isNotEmpty && middleName.isNotEmpty && lastName.isNotEmpty) {
+      final snackBar = SnackBar(
+        content: Text('Name: $firstName $middleName $lastName'),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+      final snackBar = SnackBar(
+        content: Text('Please enter all names'),
+        backgroundColor: Colors.red,
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('User Details App'),
       ),
-      body: Center(
-        child: Column(
+      body:
+      Padding(
+        padding: const EdgeInsets.all(16),
+        child:
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 150),
+          children: [
+            Image.asset(
+              'C:/Users/USER/desktop/herta.gif', // Replace with the URL of your GIF
+              width: 250,
+              height: 250,),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8),
               child: TextField(
                 controller: firstNameController,
                 decoration: InputDecoration(
-                  labelText: "Enter First Name",
+                  labelText: 'First Name',
                   border: OutlineInputBorder(),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 150),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8),
               child: TextField(
                 controller: middleNameController,
                 decoration: InputDecoration(
-                  labelText: "Enter Middle Name",
+                  labelText: 'Middle Name',
                   border: OutlineInputBorder(),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 150),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8),
               child: TextField(
                 controller: lastNameController,
                 decoration: InputDecoration(
-                  labelText: "Enter Last Name",
+                  labelText: 'Last Name',
                   border: OutlineInputBorder(),
                 ),
               ),
             ),
-            Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 150),
-                child: ElevatedButton(
-                  onPressed: (){
-                    FlutterToastr.show("Hello ${firstNameController.text} ${middleNameController.text} ${lastNameController.text}!",
-                        context, duration: FlutterToastr.lengthLong, position:  FlutterToastr.top, backgroundColor: Colors.orangeAccent);
-                    },
-                  child: const Text("Display Name"),
-                )),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: (){
+                FlutterToastr.show("Hello ${firstNameController.text} ${middleNameController.text} ${lastNameController.text} !",
+                    context, duration: FlutterToastr.lengthLong, position:  FlutterToastr.top, backgroundColor: Colors.purpleAccent);
+
+              },
+              child: Text('Show Details'),
+            ),
           ],
         ),
-
-      ),
-
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-        child:Stack(
-          alignment: Alignment.bottomRight,
-          children: [
-            Positioned(
-                right: 0,
-                child: FloatingActionButton(
-                  onPressed: _incrementCounter,
-                  tooltip: "Add",
-                  child: const Icon(Icons.add),
-                )),
-            Positioned(
-                right: 80,
-                child: FloatingActionButton(
-                  onPressed: _minusCounter,
-                  tooltip: "Deduct",
-                  child: const Icon(Icons.remove),
-                )),
-          ],
-        )
-
       ),
     );
   }
